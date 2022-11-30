c44fcc87e1b9e6d5bc9e77712dd81c31c594c6d0
Sarah Nix <73567871+sarahnix@users.noreply.github.com>
2022-11-30 17:07:41 -0500

    Update Contact.js

--- client/charitables/src/routes/Contact.js
+++ client/charitables/src/routes/Contact.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Contact() {
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/83857590-70fa-11ed-a377-655c67143cec"; 
+
+const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
     return (
-        <div className="contact">
-            <h1>Contact Us</h1>
-        </div>
+      <>
+        <h2>Thank you for your message!</h2>
+      </>
     );
-}
+  }
+
+  return (
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
+      <div>
+        <input type="text" placeholder="Your name" name="name" required />
+      </div>
+      <div>
+        <input type="email" placeholder="Email" name="email" required />
+      </div>
+      <div>
+        <textarea placeholder="Your message" name="message" required />
+      </div>
+      <div>
+        <button type="submit"> Send a message </button>
+      </div>
+    </form>
+  );
+};
 
 export default Contact;
